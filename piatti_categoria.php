3383343f8eece3f2e1dd34ccb60b5ec6bf123800
federica <federicarsl@gmail.com>
2012-03-26 14:13:48 +0200

    aggiornamento

--- piatti_categoria.php
+++ piatti_categoria.php
@@ -1,5 +1,4 @@
 <?php $categoria_cibi= getCibiFromCategoria($id_categoria); ?>
-	
 <?php foreach($categoria_cibi as $cibo): ?>
 	<a href="index.php?action=scelta_piatto&id_cibo=<?php echo $cibo['id'] ?>">
 		<img class="piatto_img" src="images/<?php echo $cibo['immagine'] ?>" alt="<?php echo $cibo['descrizione'] ?>" title="<?php echo $cibo['descrizione'] ?>"/>
