5cc42b0e4992add0bbb0b26cf80cfaa2fcbb2ae5
federica <federicarsl@gmail.com>
2012-03-28 20:03:43 +0200

    Aggiornamento corretto, aggiunta comanda tavolo e pulsanti. Modificato css index,

--- dettaglio_piatto.php
+++ dettaglio_piatto.php
@@ -4,31 +4,21 @@
 ?>
 <div id='dettaglio'>
 	<h3><?php echo $cibo ['descrizione']?></h3>
-		<a href="index.php?action=scelta_piatto&id_cibo=<?php echo $cibo['id'] ?>"></a>
 		<img class="piatto_img2" src="images/<?php echo $cibo['immagine'] ?>" alt= "<?php echo $cibo['descrizione'] ?>"/>
 	<h3> € <?php echo $cibo ['costo']?></h3>	
 	<form> 
 		<fieldset>
-			<legend>Quantità</legend>
+			<legend>Aggiungi</legend>
 			<select name="Quantità" >
-				<option value="ordina.php" selected="selected">1  </option>
-				<option value="ordina.php">2</option>
-				<option value="ordina.php">3</option>
-				<option value="ordina.php">4</option>
-				<option value="ordina.php">5</option>
-				<option value="ordina.php">6</option>
-				<option value="ordina.php">7</option>
-				<option value="ordina.php">8</option>
-				<option value="ordina.php">9</option>
-				<option value="ordina.php">10</option>
+				<option value="ordina.php">1</option>
 			</select>	
 			<input type="submit" value="Aggiungi all'ordine"/>
 		</fieldset>
 	</form> 
- </div> 
- <div id="ordine_tav">
-	 <form action="ordine_tavolo.php">
+</div> 
+<div id="comanda_tav">
+	 <form action="comanda_tavolo.php">
 	   <input type="submit" value="IL TUO ORDINE"/>
 	 </form> 
- </div>
+</div>
 
