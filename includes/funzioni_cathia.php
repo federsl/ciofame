5dafd99d844f99da5723e1551623f52affcc50cb
federica <federicarsl@gmail.com>
2012-04-05 12:25:33 +0200

    Aggiornamento funzioni cathia

--- includes/funzioni_cathia.php
+++ includes/funzioni_cathia.php
@@ -8,9 +8,9 @@
 		$cibo = mysql_fetch_array($risultato, MYSQL_ASSOC); 
 		return $cibo; 
 	} 
-	function getOrdineaperto (){
-		$query= "SELECT * FROM ordine where stato= 0";
-		$risultato= mysql_query($query) or die ('Query Fallita: '.$query);
+	function getOrdiniAperti (){
+		$query= "SELECT * FROM one where stato= 0";
+		$risultato= mysql_query($queryrdi) or die ('Query Fallita: '.$query);
 		$risposta = array();
 		while  ($ordine= mysql_fetch_array ($risultato, MYSQL_ASSOC))	
 		{
@@ -18,21 +18,21 @@
 		}
 		return $risposta;							
 	}
-	function get_ordine_in_preparazione (){
-		$query= "SELECT  ordine FROM ordine_has_cibo where stato=1";
+	function getCibiInPreparazione (){
+		$query= "SELECT  cibo_id FROM ordine_has_cibo where stato=1";
 		$risultato= mysql_query ($query) or ('Query Fallita: '.$query);
 		$risposta = array ();
-		while ($ordine= mysql_fatch_array ($risultato, MYSQL_ASSOC));
+		while ($ordine= mysql_fatch_array ($risultato, MYSQL_ASSOC))
 		{
 			$risposta[]= $ordine;		
 		}
 		return $risposta;	
 	}
-	function get_ordine_pronto (){
-		$query= "SELECT  ordine FROM ordine_has_cibo where stato=2";
+	function getCibiPronti (){
+		$query= "SELECT  cibo_id FROM ordine_has_cibo where stato=2";
 		$risultato= mysql_query ($query) or ('Query Fallita: '.$query);
 		$risposta = array ();
-		while ($ordine= mysql_fatch_array ($risultato, MYSQL_ASSOC));
+		while ($ordine= mysql_fatch_array ($risultato, MYSQL_ASSOC))
 		{	
 			$risposta[]= $ordine;
 		}
