09ec286ee06b26b6e56d9d46da29f3717f1e03b2
federica <federicarsl@gmail.com>
2012-04-05 22:56:50 +0200

    Completamento front/end (manca la parte grafica)

--- includes/funzioni_cathia.php
+++ includes/funzioni_cathia.php
@@ -1,11 +1,6 @@
 <?php 
 	include 'funzioni_db.php';
-	function getCiboFromId($id) { 
-		$query = "SELECT * FROM cibo WHERE id=$id"; 
-		$risultato = mysql_query($query) or die('Richiesta fallita' . $query);   
-		$cibo = mysql_fetch_array($risultato, MYSQL_ASSOC); 
-		return $cibo; 
-	} 
+	
 	function getOrdiniAperti (){
 		$query= "SELECT * FROM ordine where stato= 0";
 		$risultato= mysql_query($query) or die ('Query Fallita: '.$query);
@@ -16,8 +11,9 @@
 		}
 		return $risposta;							
 	}
-	function getCibiInPreparazione (){
-		$query= "SELECT  cibo_id FROM ordine_has_cibo where stato=1";
+	
+	function getCibiInAttesa ($id_ordine){
+		$query= "SELECT  cibo_id FROM ordine_has_cibo where stato=0 and ordine_id=$id_ordine";
 		$risultato= mysql_query ($query) or ('Query Fallita: '.$query);
 		$risposta = array ();
 		while ($ordine= mysql_fetch_array ($risultato, MYSQL_ASSOC))
@@ -26,8 +22,19 @@
 		}
 		return $risposta;	
 	}
-	function getCibiPronti (){
-		$query= "SELECT  cibo_id FROM ordine_has_cibo where stato=2";
+	
+	function getCibiInPreparazione ($id_ordine){
+		$query= "SELECT  cibo_id FROM ordine_has_cibo where stato=1 and ordine_id=$id_ordine";
+		$risultato= mysql_query ($query) or ('Query Fallita: '.$query);
+		$risposta = array ();
+		while ($ordine= mysql_fetch_array ($risultato, MYSQL_ASSOC))
+		{
+			$risposta[]= $ordine;		
+		}
+		return $risposta;	
+	}
+	function getCibiPronti ($id_ordine){
+		$query= "SELECT  cibo_id FROM ordine_has_cibo where stato=2 and ordine_id=$id_ordine";
 		$risultato= mysql_query ($query) or ('Query Fallita: '.$query);
 		$risposta = array ();
 		while ($ordine= mysql_fetch_array ($risultato, MYSQL_ASSOC))
--- ordine.php
+++ ordine.php
@@ -1,11 +1,14 @@
 <?php 
-$ordine_inserito= inserisciOrdine ();
-foreach ($_SESSION['ordine'] as $id_cibo => $quantita)
+if(isset($_SESSION['ordine']))
 {
-	inserisciCiboInOrdine($ordine_inserito, $id_cibo);
+	$ordine_inserito= inserisciOrdine ();
+	foreach ($_SESSION['ordine'] as $id_cibo => $quantita)
+	{
+		inserisciCiboInOrdine($ordine_inserito, $id_cibo);
+	}
+	unset ($_SESSION['ordine']);
+	$_SESSION['ordine_id']= $ordine_inserito;
 }
-unset ($_SESSION['ordine']);
-$_SESSION['ordine']= $ordine_inserito;
 include 'stato_ordine.php';
 ?>
 
