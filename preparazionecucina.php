437c144ffc1dc1bdcc1e47c80150328f2261704e
Cathia <infodimensionesole@gmail.com>
2012-04-13 10:30:46 +0200

    Eliminazione elenchi puntati e inserimento elenco normale

--- preparazionecucina.php
+++ preparazionecucina.php
@@ -1,8 +1,8 @@
 <html>
 	<head></head>
-		<title>Ordini Cucina</title>
+		<title>Cucina</title>
 		
-			<link rel="stylesheet" type="text/css" href="css/backend.css">
+			<link rel="stylesheet" type="text/css" href="backend.css">
 				
 		<body>
 		
@@ -11,61 +11,62 @@
 						
 					<?php 
 						include_once 'includes/funzioni_cathia.php';
+						include_once 'includes/funzioni_fede.php';
 						
 						if(isset($_GET['id_ordine']) && isset($_GET['id_cibo']))
 							aggiornaStatoCiboInOrdine($_GET['id_ordine'], $_GET['id_cibo']);
 							
 						$ordini_aperti = getOrdiniAperti();
-						////includere ordini_in_cucina
+						
 					?> 
 					
 				
-			<table id="principale" width="865" align="center" height="154" border="1" cellpadding="2">
+			<table width="800" align="center" height="154" border="4" cellpadding="2">
 				<tr>
 					<td width="259" align="center"><strong id="ordine">In Attesa</strong></td>
 					<td width="259" align="center"><strong id="ordine">In Preparazione</strong></td>
 					<td width="259" align="center"><strong id="ordine">Pronto </strong></td>
 				</tr>
-				<?php foreach($ordini_aperti as $ordine) { // prende tutti gli ordini aperti//?> 
+				<?php foreach($ordini_aperti as $ordine){ // prende tutti gli ordini aperti//?> 
 				<tr>
 					<td>
-						<ul>
-						<?php foreach(getCibiInAttesa($ordine['id']) as $cibo){//per ogni ordine aperto stampa tutti i cibi che trova in attesa//
-										$cibo=getCiboFromId($cibo['cibo_id']);
+						<dl>
+						<?php foreach(getCibiInAttesa($ordine['id']) as $cibo_in_attesa){//per ogni ordine aperto stampa tutti i cibi che trova in attesa//
+										$cibo=getCiboFromId($cibo_in_attesa['cibo_id']);
 						?>
-							<li>
+							<dt>
 								<a href="preparazionecucina.php?action=prontocucina&id_ordine=<?php echo $ordine['id']?>&id_cibo=<?php echo $cibo['id'];?>">
-									<img class="fotopronto"src="images/<?php echo $cibo['immagine']?>">
+									<img class="fotopronto"src="images/<?php echo $cibo['immagine']?>" width="100" height="80">
 								</a>
-							</li>
+							</dt>
 						<?php } ?>
-						</ul>
+						</dl>
 					</td>
 					<td>
-						<ul>
+						<dl>
 						<?php foreach(getCibiInPreparazione($ordine['id']) as $cibo){//per ogni ordine aperto stampa tutti i cibi che trova in attesa//
 										$cibo=getCiboFromId($cibo['cibo_id']);
 						?>
-							<li>
+							<dt>
 								<a href="preparazionecucina.php?action=prontocucina&id_ordine=<?php echo $ordine['id']?>&id_cibo=<?php echo $cibo['id'];?>">
-									<img class="fotopronto"src="images/<?php echo $cibo['immagine']?>">
+									<img class="fotopronto" src="images/<?php echo $cibo['immagine']?>" width="100" height="80">
 								</a>
-							</li>
+							</dt>
 						<?php } ?>
-						</ul>
+						</dl>
 					</td>
 					<td>
-						<ul>
+						<dl>
 						<?php foreach(getCibiPronti($ordine['id']) as $cibo){//per ogni ordine aperto stampa tutti i cibi che trova in attesa//
 										$cibo=getCiboFromId($cibo['cibo_id']);
 						?>
-							<li>
+							<dt>
 								<a href="preparazionecucina.php?action=prontocucina&id_ordine=<?php echo $ordine['id']?>&id_cibo=<?php echo $cibo['id'];?>">
-									<img class="fotopronto"src="images/<?php echo $cibo['immagine']?>">
+									<img class="pronto" src="images/<?php echo $cibo['immagine']?>" width="120" height="100">
 								</a>
-							</li>
+							</dt>
 						<?php } ?>
-						</ul>
+						</dl>
 					</td>
 				</tr>
 				<?php } ?>
